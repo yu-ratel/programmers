676ba8ac4054b8dbac2745766e03eb437e8d402e
yu-retel <dbdudals100@naver.com>
2023-03-16 19:49:33 +0900

    Feat : Js Lv2 귤 고르기 문제풀이

---
+++ Lv2.js/괄호 회전하기.js
@@ -0,0 +1,10 @@
+function solution(m, n, board) {
+  let answer = 0;
+  return answer;
+}
+
+let m = 5;
+let n = 6;
+let board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"];
+
+console.log(solution(m, n, board));
