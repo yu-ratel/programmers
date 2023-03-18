a75ef9fd4bba127a19060863b87eb87e022da1ae
yu-ratel <dbdudals100@gmail.com>
2023-03-18 16:39:13 +0900

    feat: Js Lv2 문제풀이

--- Lv2.js/괄호 회전하기.js
+++ Lv2.js/괄호 회전하기.js
@@ -1,10 +1,38 @@
-function solution(m, n, board) {
+function solution(s) {
   let answer = 0;
+  let list = s.split("");
+  const open = ["[", "(", "{"];
+  const close = ["]", ")", "}"];
+  for (let i = 0; i < s.length; i++) {
+    let cnt = 0;
+    let stack = [];
+    while (cnt != list.length) {
+      if (close.includes(list[0])) {
+        stack.push(1);
+        break;
+      }
+      if (open.includes(list[cnt])) {
+        stack.push(list[cnt]);
+        cnt++;
+      } else {
+        if (
+          (stack[stack.length - 1] === "[" && list[cnt] === "]") ||
+          (stack[stack.length - 1] === "(" && list[cnt] === ")") ||
+          (stack[stack.length - 1] === "{" && list[cnt] === "}")
+        ) {
+          stack.pop();
+          cnt++;
+        } else {
+          stack.push(list[cnt]);
+          cnt++;
+        }
+      }
+    }
+    if (stack.length === 0) answer++;
+    list.push(list.shift());
+  }
   return answer;
 }
 
-let m = 5;
-let n = 6;
-let board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"];
-
-console.log(solution(m, n, board));
+let s = "[](){}";
+console.log(solution(s));
