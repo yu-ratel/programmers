2c9a26c24f0f3af0f986b1f56cff1fd867badb5f
yu-ratel <dbdudals100@gmail.com>
2023-07-21 02:16:30 +0900

    feat: Js Lv2 문제풀이

--- Lv2.js/이모티콘 할인행사.js
+++ Lv2.js/이모티콘 할인행사.js
@@ -1,45 +1,58 @@
 function solution(users, emoticons) {
-  let [join, sales] = [0, 0];
-  let discount = [10, 20, 30, 40];
+  const list = [];
+  const discount = [10, 20, 30, 40];
+  const arr = [];
 
-  users.map((user) => {
-    let list = [];
-    let sum = [];
-    discount.map((el) => {
-      el >= user[0] ? list.push([el, user[1]]) : null;
-    });
-    list.map((el) => {
-      let max = [];
-      let dis = 1 - el[0] / 100;
-      emoticons.map((emoticon) => max.push(emoticon * dis));
-
-      el[1] = Math.max(...max);
-    });
-
-    list = list.sort((a, b) => a[1] - b[1]);
-
-    if (list.length > 1) {
-      sum.push(list.pop()[1] + list.pop()[1]);
-      if (sum >= user[1]) join++;
-      else sales = sum;
-    } else {
-      if (list[0][1] >= user[1]) join++;
-      else sales = list[0][1];
+  function DFS(idx) {
+    if (idx === emoticons.length) return list.push([...arr]);
+    for (let i = 0; i < discount.length; i++) {
+      arr[idx] = discount[i];
+      DFS(idx + 1);
     }
+  }
+
+  DFS(0);
+
+  let answer = [];
+  list.map((el) => {
+    let [join, totalSum] = [0, 0];
+
+    users.map((user) => {
+      let [rate, money] = [user[0], user[1]];
+      let sum = 0;
+      let flag = false;
+
+      emoticons.map((price, priceIdx) => {
+        if (el[priceIdx] >= rate) {
+          sum += (price * (100 - el[priceIdx])) / 100;
+        }
+        if (sum >= money) {
+          flag = true;
+          return;
+        }
+      });
+      flag ? join++ : (totalSum += sum);
+    });
+    answer.push([join, totalSum]);
   });
 
-  return [join, sales];
+  answer.sort((a, b) => {
+    if (a[0] > b[0]) return -1;
+    if (a[0] < b[0]) return 1;
+
+    return b[1] - a[1];
+  });
+
+  return answer[0];
 }
 
 let users = [
-  [40, 2900],
-  [23, 10000],
-  [11, 5200],
-  [5, 5900],
-  [40, 3100],
-  [27, 9200],
-  [32, 6900],
+  [40, 10000],
+  [25, 10000],
 ];
-let emoticons = [1300, 1500, 1600, 4900];
+let emoticons = [7000, 9000];
 
 console.log(solution(users, emoticons));
+
+// DFS로 모든 할인율 경우의수를 구하고 거기서 계산하여 문제에 주어진 조건대로 정렬 한 문제
+// 시간이 여유로우므로 (이걸 판단할 수 있는 능력 - 문제 조건범위 파악) 꾸역꾸역 풀면된다.
