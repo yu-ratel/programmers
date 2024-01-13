d319b13f90ba6207490da7c58b32b0f130307dd4
yu-ratel <dbdudals100@naver.com>
2024-01-13 22:20:52 +0900

    feat: Js Lv0 문제풀이 (1)

---
+++ Lv0/두 수의 합.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+  return (BigInt(a) + BigInt(b)) + '';
+}
+
+let a = "682";
+let b = "734";
+
+console.log(solution(a, b));
+
+//출처  (BigInt)
+//https://cocobi.tistory.com/154
