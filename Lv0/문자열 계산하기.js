fb214579115a558b4c332aa2d79cf0726b3a552e
yu-ratel <dbdudals100@gmail.com>
2024-01-13 22:21:31 +0900

    refactor: Js Lv0 문제풀이누락 추가 (1)

--- Lv0/문자열 계산하기.js
+++ Lv0/문자열 계산하기.js
@@ -1,13 +1,17 @@
 function solution(my_string) {
-  let arr = my_string.split(' ')
-  let cal = arr[0] * 1;
-  for(let i = 1; i< arr.length; i++){
-      if(arr[i] === '+')
-          cal += arr[i+1] * 1;
-      else if(arr[i] === '-')
-          cal -= arr[i+1] * 1;
-      else
-          continue;
+  let arr = my_string.split(' ');
+
+  let tmp = arr[0] * 1;
+
+  for (let i = 1; i < arr.length; i += 1){
+    if(arr[i] === '+') {
+      tmp += arr[i + 1] * 1;
+    }
+     if(arr[i] === '-') {
+      tmp -= arr[i + 1] * 1;
+    }
+
+    else continue;
   }
-  return cal;
+  return tmp;
 }
