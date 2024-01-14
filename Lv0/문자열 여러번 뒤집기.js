b92a7589ee1b89a770c7ae5091189c8b525b130e
yu-ratel <dbdudals100@naver.com>
2024-01-14 23:58:53 +0900

    feat: Js Lv0 문제풀이 (1)

---
+++ Lv0/문자열 여러번 뒤집기.js
@@ -0,0 +1,17 @@
+function solution(my_string, queries) {
+  const answer = my_string.split('');
+
+  queries.forEach((arr) => {
+    const [start, end] = arr;
+    const reverseStr = answer.slice(start, end + 1).reverse().join('');
+
+    answer.splice(start, end - start + 1, ...reverseStr.split(''));
+  })
+
+  return answer.join('');
+}
+
+let my_string = "rermgorpsam";
+let queries = [[2, 3], [0, 7], [5, 9], [6, 10]];
+
+console.log(solution(my_string, queries));
