362331fd64f84f0dc11aceda4aea884994f22715
yu-ratel <dbdudals100@gmail.com>
2024-02-08 23:55:38 +0900

    feat: Js Lv0 문제풀이 (1)

---
+++ Lv0/하/배열 만들기2.js
@@ -0,0 +1,18 @@
+function solution(l, r) {
+  const answer = [];
+
+  for (let i = l; i <= r; i += 5 ) {
+    let arr = (i + '').split('');
+
+    if(arr.every((num) => num === '0' || num === '5')) {
+      answer.push(i);
+    }
+  }
+
+  return answer.length ?  answer : [-1];
+}
+
+let l = 5;
+let r = 555;
+
+console.log(solution(l, r));
