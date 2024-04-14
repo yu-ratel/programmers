8f958e354f763653598ad97a5cc9d383bc8e2a4e
yu-ratel <dbdudals100@naver.com>
2024-04-14 20:44:41 +0900

    feat: Js Lv1 문제 다시 풀이 (1)

--- Lv1.js/크기가 작은 부분문자열.js
+++ Lv1.js/크기가 작은 부분문자열.js
@@ -1,14 +1,31 @@
 function solution(t, p) {
-    let answer = 0;
-    let cut = p.length;
-    for (let i = 0; i <t.length - cut; i++) {
-        let num = t.slice(i, i + cut);
-        if (Number(num) <= Number(p)) answer++;
-    }   
-    return answer;
+  let answer = 0;
+  let cut = p.length;
+  for (let i = 0; i < t.length - cut; i++) {
+    let num = t.slice(i, i + cut);
+    if (Number(num) <= Number(p)) answer++;
+  }
+  return answer;
 }
 
 let t = "3141592";
-let p =	"271";
+let p = "271";
+
+console.log(solution(t, p));
+
+// 다시 풀기
+function solution(t, p) {
+  const cutLength = p.length;
+  const substrings = t
+    .split("")
+    .map(
+      (_, idx) => idx <= t.length - cutLength && t.slice(idx, idx + cutLength)
+    );
+
+  return substrings.filter((num) => num <= p && num !== false).length;
+}
+
+let t = "3141592";
+let p = "271";
 
 console.log(solution(t, p));
